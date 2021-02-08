66b33a260e75e2276769118e91861dded2c2d0f6
hanna kebede <se.hanna.kebede@gmail.com>
2021-02-08 15:05:40 +0300

    working on edit task

--- Task Manger IDB/assets/js/app.js
+++ Task Manger IDB/assets/js/app.js
@@ -1,58 +1,49 @@
-// Define UI Variables 
-const taskInput = document.querySelector('#task'); //the task input text field
-const form = document.querySelector('#task-form'); //The form at the top
-const filter = document.querySelector('#filter'); //the task filter text field
-const taskList = document.querySelector('.collection'); //The UL
-const clearBtn = document.querySelector('.clear-tasks'); //the all task clear button
-
-const reloadIcon = document.querySelector('.fa'); //the reload button at the top navigation 
+const taskInput = document.querySelector('#task'); 
+const form = document.querySelector('#task-form');  
+const filter = document.querySelector('#filter');                  
+const taskList = document.querySelector('.collection'); 
+const clearBtn = document.querySelector('.clear-tasks');   
+//the reload button at the top right of navigation
+const reloadIcon = document.querySelector('.fa');  
 const orderBtn = document.querySelector('select');
 
-
-
-//DB variable 
-
+// DB variable
 let DB;
 
-
-
-// Add Event Listener [on Load]
+// add event listener [on load]
 document.addEventListener('DOMContentLoaded', () => {
     // create the database
     let TasksDB = indexedDB.open('tasks', 1);
 
-    // if there's an error
-    TasksDB.onerror = function() {
-            console.log('There was an error');
-        }
-        // if everything is fine, assign the result to the instance
+    // if everything is fine, assign the result to the instance (DB)
     TasksDB.onsuccess = function() {
-        console.log('Database Ready');
-
+        // console.log('Database ready');
         // save the result
         DB = TasksDB.result;
-
-        // display the Task List 
+        // display the Task List
         displayTaskList();
-    }
+    };
+
+    // if there's an error
+    TasksDB.onerror = function() {
+        console.log('There was an error')
+    };
 
     // This method runs once (great for creating the schema)
     TasksDB.onupgradeneeded = function(e) {
         // the event will be the database
         let db = e.target.result;
-
-        // create an object store, 
-        // keypath is going to be the Indexes
-        let objectStore = db.createObjectStore('tasks', { keyPath: 'id', autoIncrement: true });
+        // keyPath is going to be the indices
+        let objectStore = db.createObjectStore('tasks', {keyPath: 'id', autoIncrement: true});
 
         // createindex: 1) field name 2) keypath 3) options
-        objectStore.createIndex('taskname', 'taskname', { unique: false });
+        objectStore.createIndex('taskname', 'taskname', {unique: false});
+        objectStore.createIndex('date',  'date', {unique: false});
 
         console.log('Database ready and fields created!');
     }
 
     form.addEventListener('submit', addNewTask);
-
     function addNewTask(e) {
         e.preventDefault();
 
@@ -62,62 +53,57 @@ document.addEventListener('DOMContentLoaded', () => {
 
             return;
         }
-
+        
         // create a new object with the form info
         let newTask = {
             taskname: taskInput.value,
             date: Date.now()
         }
-
         // Insert the object into the database 
         let transaction = DB.transaction(['tasks'], 'readwrite');
         let objectStore = transaction.objectStore('tasks');
-
+      
         let request = objectStore.add(newTask);
-
         // on success
         request.onsuccess = () => {
             form.reset();
         }
         transaction.oncomplete = () => {
-            console.log('New appointment added');
-
+            console.log('New task added');
             displayTaskList();
         }
-        transaction.onerror = () => {
-            console.log('There was an error, try again!');
+        transaction.onerror = () => { 
+            console.log('There was an error, try again!'); 
         }
-
     }
-
+      
     function displayTaskList() {
         // clear the previous task list
-        while (taskList.firstChild) {
+        while (taskList.firstChild) {   
             taskList.removeChild(taskList.firstChild);
         }
-
+      
         // create the object store
         let objectStore = DB.transaction('tasks').objectStore('tasks');
-
+      
         objectStore.openCursor().onsuccess = function(e) {
             // assign the current cursor
             let cursor = e.target.result;
-
+      
             if (cursor) {
-
                 // Create an li element when the user adds a task 
                 const li = document.createElement('li');
-                //add Attribute for delete 
-                li.setAttribute('data-task-id', cursor.value.id);
-                // Adding a class
                 li.className = 'collection-item';
+                // add attribute for deletion / removeTask
+                li.setAttribute('data-task-id', cursor.value.id);
                 // Create text node and append it 
                 li.appendChild(document.createTextNode(cursor.value.taskname));
                 // Create new element for the link 
                 const link = document.createElement('a');
                 // Add class and the x marker for a 
                 link.className = 'delete-item secondary-content';
-                link.innerHTML = `<i class="fa fa-remove"></i>
+                link.innerHTML = `
+                 <i class="fa fa-remove"></i>
                 &nbsp;
                 <a href="edit.html?id=${cursor.value.id}"><i class="fa fa-edit"></i> </a>
                 `;
@@ -125,11 +111,12 @@ document.addEventListener('DOMContentLoaded', () => {
                 li.appendChild(link);
                 // Append to UL 
                 taskList.appendChild(li);
+
                 cursor.continue();
             }
         }
     }
-
+      
     //clear button event listener    
     clearBtn.addEventListener('click', clearAllTasks);
     //clear tasks 
@@ -145,12 +132,10 @@ document.addEventListener('DOMContentLoaded', () => {
     
         console.log("Tasks Cleared !!!");
     }
-
-    // Remove task event [event delegation]
+    
     taskList.addEventListener('click', removeTask);
-
+    // remove a task
     function removeTask(e) {
-
         if (e.target.parentElement.classList.contains('delete-item')) {
             if (confirm('Are You Sure about that ?')) {
                 // get the task id
@@ -159,15 +144,12 @@ document.addEventListener('DOMContentLoaded', () => {
                 let transaction = DB.transaction(['tasks'], 'readwrite');
                 let objectStore = transaction.objectStore('tasks');
                 objectStore.delete(taskID);
-
+    
                 transaction.oncomplete = () => {
                     e.target.parentElement.parentElement.remove();
                 }
-
             }
-
         }
-
     }
 
     // ascending and descending sorting order
@@ -268,6 +250,3 @@ document.addEventListener('DOMContentLoaded', () => {
     
 });
 
-
-
-});
